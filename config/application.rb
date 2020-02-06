c04ae2214d5f729498099aad59dd6f28ce109a98
filipdraganic <draganicfilip@gmail.com>
2020-02-06 11:45:48 +0100

    dodata linija u config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module RubyProjekat
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.initialize_on_precompile = false
 
     config.generators do |g|
       g.test_framework :minitest, spec: true
