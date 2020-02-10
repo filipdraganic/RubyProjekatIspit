f5547ca619476ef22d29a7ba80430b7d09a95756
filipdraganic <draganicfilip@gmail.com>
2020-02-11 00:14:39 +0100

    dodat .yarnclean

--- db/migrate/20200203204654_add_user_to_posts.rb
+++ db/migrate/20200203204654_add_user_to_posts.rb
@@ -1,6 +1,6 @@
 class AddUserToPosts < ActiveRecord::Migration[6.0]
   def change
-    add_column :posts, :created_by, :user
+    add_column :posts, :created_by, :integer
     add_reference :posts, :user, foreign_key: true
   end
 end
